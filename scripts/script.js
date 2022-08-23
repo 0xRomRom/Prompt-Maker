04c4906b8989e446724aeb7b61c4af31b9577bc8
0xromrom <roman0811@live.nl>
2022-08-22 18:54:45 -0700

    Few media queries added

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+const box7 = document.querySelector(".subjects-7");
+const box6 = document.querySelector(".subjects-6");
+const box8 = document.querySelector(".subjects-8");
+const promptString = document.querySelector(".prompt-string");
