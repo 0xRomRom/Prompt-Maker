11f828bc60b69342ae1254c4f76e435df27f5ad8
0xromrom <roman0811@live.nl>
2022-08-23 13:54:54 -0700

    Code refactor

--- scripts/domMenusModule.js
+++ scripts/domMenusModule.js
@@ -2,12 +2,7 @@
 
 //Menu 1: Subject
 
-const subjectBoxes = document.querySelector(".modal-boxes");
-const dot1 = document.querySelector(".d1");
-const dot2 = document.querySelector(".d2");
-const dot3 = document.querySelector(".d3");
-
-const subPage1 = `<div class="subjects-1 subjects-style">
+export const subPage1 = `<div class="subjects-1 subjects-style">
 <div class="modal-name-style">Humans</div>
 <img src="img/index/Humans.png" class="modal-img-style" />
 </div>
@@ -38,10 +33,10 @@ const subPage1 = `<div class="subjects-1 subjects-style">
 <div class="subjects-8 subjects-style">
 <div class="modal-name-style">Humans</div>
 <img src="img/index/Humans.png" class="modal-img-style" />
-</div>`;
-subjectBoxes.innerHTML = subPage1;
+</div>
+`;
 
-const subPage2 = `<div class="subjects-5 subjects-style">
+export const subPage2 = `<div class="subjects-5 subjects-style">
 <div class="modal-name-style">Items</div>
 <img src="img/index/Items.png" class="modal-img-style" />
 </div>
@@ -73,7 +68,7 @@ const subPage2 = `<div class="subjects-5 subjects-style">
 <img src="img/index/Buildings.png" class="modal-img-style" />
 </div>`;
 
-const subPage3 = `<div class="subjects-2 subjects-style">
+export const subPage3 = `<div class="subjects-2 subjects-style">
 <div class="modal-name-style">Animals</div>
 <img src="img/index/Animals.png" class="modal-img-style" />
 </div>
@@ -103,36 +98,3 @@ const subPage3 = `<div class="subjects-2 subjects-style">
 <div class="modal-name-style">Animals</div>
 <img src="img/index/Animals.png" class="modal-img-style" />
 </div>`;
-
-dot1.addEventListener("click", () => {
-  dot1.style.backgroundColor = "rgb(37, 49, 96)";
-  dot2.style.backgroundColor = "rgb(8, 11, 22)";
-  dot3.style.backgroundColor = "rgb(8, 11, 22)";
-  subjectBoxes.innerHTML = subPage1;
-  subjectBoxes.classList.add("fadeOut");
-  setTimeout(() => {
-    subjectBoxes.classList.remove("fadeOut");
-  }, 500);
-});
-
-dot2.addEventListener("click", () => {
-  dot1.style.backgroundColor = "rgb(8, 11, 22)";
-  dot2.style.backgroundColor = "rgb(37, 49, 96)";
-  dot3.style.backgroundColor = "rgb(8, 11, 22)";
-  subjectBoxes.innerHTML = subPage2;
-  subjectBoxes.classList.add("fadeOut");
-  setTimeout(() => {
-    subjectBoxes.classList.remove("fadeOut");
-  }, 500);
-});
-
-dot3.addEventListener("click", () => {
-  dot1.style.backgroundColor = "rgb(8, 11, 22)";
-  dot2.style.backgroundColor = "rgb(8, 11, 22)";
-  dot3.style.backgroundColor = "rgb(37, 49, 96)";
-  subjectBoxes.innerHTML = subPage3;
-  subjectBoxes.classList.add("fadeOut");
-  setTimeout(() => {
-    subjectBoxes.classList.remove("fadeOut");
-  }, 500);
-});
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,76 @@
 "use strict";
 
-const box7 = document.querySelector(".subjects-7");
-const box6 = document.querySelector(".subjects-6");
-const box8 = document.querySelector(".subjects-8");
-const promptString = document.querySelector(".prompt-string");
+const subjectBoxes = document.querySelector(".modal-boxes");
+const dot1 = document.querySelector(".dot-1");
+const dot2 = document.querySelector(".dot-2");
+const dot3 = document.querySelector(".dot-3");
+
+//Init//
+
+////////
+
+import { subPage1, subPage2, subPage3 } from "./domMenusModule.js";
+subjectBoxes.innerHTML = subPage1;
+const pagesArray = [subPage1, subPage2, subPage3];
+let dotArray = document.querySelectorAll(".dot");
+dotArray.forEach((dot) => {
+  dot.addEventListener("click", (e) => {
+    let dotIndex = +e.target.classList[0].slice(4) - 1;
+    subjectBoxes.innerHTML = pagesArray[dotIndex];
+    subjectBoxes.classList.add("fadeOut");
+    setTimeout(() => {
+      subjectBoxes.classList.remove("fadeOut");
+    }, 500);
+    if (e.target.classList.contains("d1")) {
+      dot1.style.backgroundColor = "rgb(37, 49, 96)";
+      dot2.style.backgroundColor = "rgb(8, 11, 22)";
+      dot3.style.backgroundColor = "rgb(8, 11, 22)";
+    }
+    if (e.target.classList.contains("d2")) {
+      dot1.style.backgroundColor = "rgb(8, 11, 22)";
+      dot2.style.backgroundColor = "rgb(37, 49, 96)";
+      dot3.style.backgroundColor = "rgb(8, 11, 22)";
+    }
+    if (e.target.classList.contains("d3")) {
+      dot1.style.backgroundColor = "rgb(8, 11, 22)";
+      dot2.style.backgroundColor = "rgb(8, 11, 22)";
+      dot3.style.backgroundColor = "rgb(37, 49, 96)";
+    }
+  });
+});
+
+// dot1.addEventListener("click", () => {
+//   if (dot1.style.backgroundColor === "rgb(37, 49, 96)") return;
+//   dot1.style.backgroundColor = "rgb(37, 49, 96)";
+//   dot2.style.backgroundColor = "rgb(8, 11, 22)";
+//   dot3.style.backgroundColor = "rgb(8, 11, 22)";
+//   subjectBoxes.innerHTML = subPage1;
+//   subjectBoxes.classList.add("fadeOut");
+//   setTimeout(() => {
+//     subjectBoxes.classList.remove("fadeOut");
+//   }, 500);
+// });
+
+// dot2.addEventListener("click", () => {
+//   if (dot2.style.backgroundColor === "rgb(37, 49, 96)") return;
+//   dot1.style.backgroundColor = "rgb(8, 11, 22)";
+//   dot2.style.backgroundColor = "rgb(37, 49, 96)";
+//   dot3.style.backgroundColor = "rgb(8, 11, 22)";
+//   subjectBoxes.innerHTML = subPage2;
+//   subjectBoxes.classList.add("fadeOut");
+//   setTimeout(() => {
+//     subjectBoxes.classList.remove("fadeOut");
+//   }, 500);
+// });
+
+// dot3.addEventListener("click", () => {
+//   if (dot3.style.backgroundColor === "rgb(37, 49, 96)") return;
+//   dot1.style.backgroundColor = "rgb(8, 11, 22)";
+//   dot2.style.backgroundColor = "rgb(8, 11, 22)";
+//   dot3.style.backgroundColor = "rgb(37, 49, 96)";
+//   subjectBoxes.innerHTML = subPage3;
+//   subjectBoxes.classList.add("fadeOut");
+//   setTimeout(() => {
+//     subjectBoxes.classList.remove("fadeOut");
+//   }, 500);
+// });
