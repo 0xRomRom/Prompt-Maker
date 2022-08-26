484f51634db40fb54e5bf9aca133bb5ac785d269
0xromrom <roman0811@live.nl>
2022-08-26 15:13:31 -0700

    Menu transitions added

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,47 @@
 "use strict";
 
-// const pagesArray = [subPage1, subPage2, subPage3];
+const promptString = document.querySelector(".prompt-string");
+const copyPromptString = document.querySelector(".copy-prompt");
 const subjectBoxes = document.querySelector(".modal-boxes");
 const dot1 = document.querySelector(".dot-1");
 const dot2 = document.querySelector(".dot-2");
 const dot3 = document.querySelector(".dot-3");
-const promptString = document.querySelector(".prompt-string");
-const copyPromptString = document.querySelector(".copy-prompt");
 
 const sub1 = document.querySelector(".sub1");
 const sub2 = document.querySelector(".sub2");
 const sub3 = document.querySelector(".sub3");
 
-const subjectCounter = document.querySelector(".subject-counter");
+export const totalCounter = document.querySelector(".total-selection-counter");
+export const subjectCounter = document.querySelector(".subject-counter");
+export const locationCounter = document.querySelector(".location-counter");
+export const themesCounter = document.querySelector(".themes-counter");
+export const designCounter = document.querySelector(".design-counter");
+export const lightningCounter = document.querySelector(".lightning-counter");
+export const colorsCounter = document.querySelector(".colors-counter");
+export const cameraCounter = document.querySelector(".camera-counter");
+export const artistsCounter = document.querySelector(".artists-counter");
+export const imageCounter = document.querySelector(".image-counter");
+
 const subjectMiniCounter = document.querySelector(".subjects-counter");
-const totalCounter = document.querySelector(".total-selection-counter");
+const locationMiniCounter = document.querySelector(".locations-counter");
+const themeMiniCounter = document.querySelector(".themes-counter");
+const designMiniCounter = document.querySelector(".designs-counter");
+const lightningMiniCounter = document.querySelector(".lightnings-counter");
+const colorsMiniCounter = document.querySelector(".colors-counter");
+const cameraMiniCounter = document.querySelector(".cameras-counter");
+const artistMiniCounter = document.querySelector(".artists-counter");
+const imageMiniCounter = document.querySelector(".imagess-counter");
+
+export const optionBox1 = document.querySelector(".option-box1");
+export const optionBox2 = document.querySelector(".option-box2");
+export const optionBox3 = document.querySelector(".option-box3");
+export const optionBox4 = document.querySelector(".option-box4");
+export const optionBox5 = document.querySelector(".option-box5");
+export const optionBox6 = document.querySelector(".option-box6");
+export const optionBox7 = document.querySelector(".option-box7");
+export const optionBox8 = document.querySelector(".option-box8");
+export const optionBox9 = document.querySelector(".option-box9");
+
 //Init//
 let totalCount = 0;
 let subjectCount = 0;
