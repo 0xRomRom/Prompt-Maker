683d24e23edf5f2567bf5728cb6a5d463ccdf3fc
0xromrom <roman0811@live.nl>
2022-08-27 13:57:35 -0700

    Added all counters

--- scripts/animate.js
+++ scripts/animate.js
@@ -734,7 +734,6 @@ optionBox9.addEventListener("click", () => {
   hide(art1);
   hide(art2);
   hide(art3);
-
   hide(buttonBox);
   hide(subjectBoxes);
 
@@ -826,4 +825,161 @@ const styleReducer = (
   h1.style.color = g;
   h2.style.color = g;
   h3.style.border = gb;
+};
+
+//Render right content according to button index
+const dotArray = document.querySelectorAll(".dot");
+dotArray.forEach((dot) => {
+  dot.addEventListener("click", (e) => {
+    if (e.target.style.backgroundColor === "rgb(37, 49, 96)") return;
+    let dotIndex = +e.target.classList[0].slice(4) - 1;
+    console.log(dotIndex);
+    menuTransitions(e);
+    if (dotIndex === 0) {
+      sub1.classList.remove("hidden");
+      sub2.classList.add("hidden");
+      sub3.classList.add("hidden");
+    }
+    if (dotIndex === 1) {
+      sub1.classList.add("hidden");
+      sub2.classList.remove("hidden");
+      sub3.classList.add("hidden");
+    }
+    if (dotIndex === 2) {
+      sub1.classList.add("hidden");
+      sub2.classList.add("hidden");
+      sub3.classList.remove("hidden");
+    } //
+    if (dotIndex === 3) {
+      loc1.classList.remove("hidden");
+      loc2.classList.add("hidden");
+      loc3.classList.add("hidden");
+    }
+    if (dotIndex === 4) {
+      loc1.classList.add("hidden");
+      loc2.classList.remove("hidden");
+      loc3.classList.add("hidden");
+      sub3.classList.add("hidden");
+    }
+    if (dotIndex === 5) {
+      sub3.classList.add("hidden");
+      loc1.classList.add("hidden");
+      loc2.classList.add("hidden");
+      loc3.classList.remove("hidden");
+    } //
+    if (dotIndex === 6) {
+      the1.classList.remove("hidden");
+      the2.classList.add("hidden");
+      the3.classList.add("hidden");
+    }
+    if (dotIndex === 7) {
+      the2.classList.remove("hidden");
+      the1.classList.add("hidden");
+      the3.classList.add("hidden");
+    }
+    if (dotIndex === 8) {
+      the1.classList.add("hidden");
+      the2.classList.add("hidden");
+      the3.classList.remove("hidden");
+    } //
+    if (dotIndex === 9) {
+      des1.classList.remove("hidden");
+      des2.classList.add("hidden");
+      des3.classList.add("hidden");
+    }
+    if (dotIndex === 10) {
+      des2.classList.remove("hidden");
+      des1.classList.add("hidden");
+      des3.classList.add("hidden");
+    }
+    if (dotIndex === 11) {
+      des1.classList.add("hidden");
+      des2.classList.add("hidden");
+      des3.classList.remove("hidden");
+    } //
+    if (dotIndex === 12) {
+      lig1.classList.remove("hidden");
+      lig2.classList.add("hidden");
+      lig3.classList.add("hidden");
+    }
+    if (dotIndex === 13) {
+      lig2.classList.remove("hidden");
+      lig1.classList.add("hidden");
+      lig3.classList.add("hidden");
+    }
+    if (dotIndex === 14) {
+      lig1.classList.add("hidden");
+      lig2.classList.add("hidden");
+      lig3.classList.remove("hidden");
+    } //
+    if (dotIndex === 15) {
+      col1.classList.remove("hidden");
+      col2.classList.add("hidden");
+      col3.classList.add("hidden");
+    }
+    if (dotIndex === 16) {
+      col2.classList.remove("hidden");
+      col1.classList.add("hidden");
+      col3.classList.add("hidden");
+    }
+    if (dotIndex === 17) {
+      col1.classList.add("hidden");
+      col2.classList.add("hidden");
+      col3.classList.remove("hidden");
+    } //
+    if (dotIndex === 18) {
+      cam1.classList.remove("hidden");
+      cam2.classList.add("hidden");
+      cam3.classList.add("hidden");
+    }
+    if (dotIndex === 19) {
+      cam2.classList.remove("hidden");
+      cam1.classList.add("hidden");
+      cam3.classList.add("hidden");
+    }
+    if (dotIndex === 20) {
+      cam1.classList.add("hidden");
+      cam2.classList.add("hidden");
+      cam3.classList.remove("hidden");
+    } //
+    if (dotIndex === 21) {
+      art1.classList.remove("hidden");
+      art2.classList.add("hidden");
+      art3.classList.add("hidden");
+    }
+    if (dotIndex === 22) {
+      art2.classList.remove("hidden");
+      art1.classList.add("hidden");
+      art3.classList.add("hidden");
+    }
+    if (dotIndex === 23) {
+      art1.classList.add("hidden");
+      art2.classList.add("hidden");
+      art3.classList.remove("hidden");
+    }
+    console.log(dotIndex);
+    menuTransitions(e);
+  });
+});
+
+const menuTransitions = (e) => {
+  subjectBoxes.classList.add("fadeOut");
+  setTimeout(() => {
+    subjectBoxes.classList.remove("fadeOut");
+  }, 500);
+  if (e.target.classList.contains("d1")) {
+    dot1.style.backgroundColor = "rgb(37, 49, 96)";
+    dot2.style.backgroundColor = "rgb(8, 11, 22)";
+    dot3.style.backgroundColor = "rgb(8, 11, 22)";
+  }
+  if (e.target.classList.contains("d2")) {
+    dot1.style.backgroundColor = "rgb(8, 11, 22)";
+    dot2.style.backgroundColor = "rgb(37, 49, 96)";
+    dot3.style.backgroundColor = "rgb(8, 11, 22)";
+  }
+  if (e.target.classList.contains("d3")) {
+    dot1.style.backgroundColor = "rgb(8, 11, 22)";
+    dot2.style.backgroundColor = "rgb(8, 11, 22)";
+    dot3.style.backgroundColor = "rgb(37, 49, 96)";
+  }
 };
--- scripts/script.js
+++ scripts/script.js
@@ -65,14 +65,22 @@ const locationMiniCounter = document.querySelector(".locations-counter");
 const themeMiniCounter = document.querySelector(".themes-counter");
 const designMiniCounter = document.querySelector(".designs-counter");
 const lightningMiniCounter = document.querySelector(".lightnings-counter");
-const colorsMiniCounter = document.querySelector(".colors-counter");
+const colorsMiniCounter = document.querySelector(".colors-mini-counter");
 const cameraMiniCounter = document.querySelector(".cameras-counter");
-const artistMiniCounter = document.querySelector(".artists-counter");
+const artistMiniCounter = document.querySelector(".artists-mini-counter");
 const imageMiniCounter = document.querySelector(".imagess-counter");
 
 //Init//
 let totalCount = 0;
 let subjectCount = 0;
+let locationCount = 0;
+let themesCount = 0;
+let designCount = 0;
+let lightningCount = 0;
+let colorsCount = 0;
+let cameraCount = 0;
+let artistsCount = 0;
+let imageCount = 0;
 ////////
 
 //Copy resulting query
@@ -82,177 +90,119 @@ copyPromptString.addEventListener("click", () => {
   alert("Query Copied!");
 });
 
-//Render right content according to button index
-const dotArray = document.querySelectorAll(".dot");
-dotArray.forEach((dot) => {
-  dot.addEventListener("click", (e) => {
-    if (e.target.style.backgroundColor === "rgb(37, 49, 96)") return;
-    let dotIndex = +e.target.classList[0].slice(4) - 1;
-    console.log(dotIndex);
-    menuTransitions(e);
-    if (dotIndex === 0) {
-      sub1.classList.remove("hidden");
-      sub2.classList.add("hidden");
-      sub3.classList.add("hidden");
-    }
-    if (dotIndex === 1) {
-      sub1.classList.add("hidden");
-      sub2.classList.remove("hidden");
-      sub3.classList.add("hidden");
-    }
-    if (dotIndex === 2) {
-      sub1.classList.add("hidden");
-      sub2.classList.add("hidden");
-      sub3.classList.remove("hidden");
-    } //
-    if (dotIndex === 3) {
-      loc1.classList.remove("hidden");
-      loc2.classList.add("hidden");
-      loc3.classList.add("hidden");
-    }
-    if (dotIndex === 4) {
-      loc1.classList.add("hidden");
-      loc2.classList.remove("hidden");
-      loc3.classList.add("hidden");
-      sub3.classList.add("hidden");
-    }
-    if (dotIndex === 5) {
-      sub3.classList.add("hidden");
-      loc1.classList.add("hidden");
-      loc2.classList.add("hidden");
-      loc3.classList.remove("hidden");
-    } //
-    if (dotIndex === 6) {
-      the1.classList.remove("hidden");
-      the2.classList.add("hidden");
-      the3.classList.add("hidden");
-    }
-    if (dotIndex === 7) {
-      the2.classList.remove("hidden");
-      the1.classList.add("hidden");
-      the3.classList.add("hidden");
-    }
-    if (dotIndex === 8) {
-      the1.classList.add("hidden");
-      the2.classList.add("hidden");
-      the3.classList.remove("hidden");
-    } //
-    if (dotIndex === 9) {
-      des1.classList.remove("hidden");
-      des2.classList.add("hidden");
-      des3.classList.add("hidden");
-    }
-    if (dotIndex === 10) {
-      des2.classList.remove("hidden");
-      des1.classList.add("hidden");
-      des3.classList.add("hidden");
-    }
-    if (dotIndex === 11) {
-      des1.classList.add("hidden");
-      des2.classList.add("hidden");
-      des3.classList.remove("hidden");
-    } //
-    if (dotIndex === 12) {
-      lig1.classList.remove("hidden");
-      lig2.classList.add("hidden");
-      lig3.classList.add("hidden");
-    }
-    if (dotIndex === 13) {
-      lig2.classList.remove("hidden");
-      lig1.classList.add("hidden");
-      lig3.classList.add("hidden");
-    }
-    if (dotIndex === 14) {
-      lig1.classList.add("hidden");
-      lig2.classList.add("hidden");
-      lig3.classList.remove("hidden");
-    } //
-    if (dotIndex === 15) {
-      col1.classList.remove("hidden");
-      col2.classList.add("hidden");
-      col3.classList.add("hidden");
-    }
-    if (dotIndex === 16) {
-      col2.classList.remove("hidden");
-      col1.classList.add("hidden");
-      col3.classList.add("hidden");
-    }
-    if (dotIndex === 17) {
-      col1.classList.add("hidden");
-      col2.classList.add("hidden");
-      col3.classList.remove("hidden");
-    } //
-    if (dotIndex === 18) {
-      cam1.classList.remove("hidden");
-      cam2.classList.add("hidden");
-      cam3.classList.add("hidden");
-    }
-    if (dotIndex === 19) {
-      cam2.classList.remove("hidden");
-      cam1.classList.add("hidden");
-      cam3.classList.add("hidden");
-    }
-    if (dotIndex === 20) {
-      cam1.classList.add("hidden");
-      cam2.classList.add("hidden");
-      cam3.classList.remove("hidden");
-    } //
-    if (dotIndex === 21) {
-      art1.classList.remove("hidden");
-      art2.classList.add("hidden");
-      art3.classList.add("hidden");
-    }
-    if (dotIndex === 22) {
-      art2.classList.remove("hidden");
-      art1.classList.add("hidden");
-      art3.classList.add("hidden");
-    }
-    if (dotIndex === 23) {
-      art1.classList.add("hidden");
-      art2.classList.add("hidden");
-      art3.classList.remove("hidden");
-    }
-    console.log(dotIndex);
-    menuTransitions(e);
-  });
-});
-
-const menuTransitions = (e) => {
-  subjectBoxes.classList.add("fadeOut");
-  setTimeout(() => {
-    subjectBoxes.classList.remove("fadeOut");
-  }, 500);
-  if (e.target.classList.contains("d1")) {
-    dot1.style.backgroundColor = "rgb(37, 49, 96)";
-    dot2.style.backgroundColor = "rgb(8, 11, 22)";
-    dot3.style.backgroundColor = "rgb(8, 11, 22)";
-  }
-  if (e.target.classList.contains("d2")) {
-    dot1.style.backgroundColor = "rgb(8, 11, 22)";
-    dot2.style.backgroundColor = "rgb(37, 49, 96)";
-    dot3.style.backgroundColor = "rgb(8, 11, 22)";
-  }
-  if (e.target.classList.contains("d3")) {
-    dot1.style.backgroundColor = "rgb(8, 11, 22)";
-    dot2.style.backgroundColor = "rgb(8, 11, 22)";
-    dot3.style.backgroundColor = "rgb(37, 49, 96)";
-  }
-};
-
 //Append selection to query
 const appendToQuery = (string) => {
   promptString.value += `${string.charAt(0).toUpperCase() + string.slice(1)}, `;
 };
 
 //Subjects UI updater
-let allSubjects = document.querySelectorAll(".subjects-style");
-allSubjects.forEach((item) => {
+let subjectPick = document.querySelectorAll(".box1");
+subjectPick.forEach((item) => {
   item.addEventListener("click", () => {
     appendToQuery(item.dataset.id);
+    console.log(item.dataset.id);
     subjectCount++;
     totalCount++;
     subjectCounter.textContent = subjectCount;
     subjectMiniCounter.textContent = subjectCount;
+    totalCounter.textContent = totalCount;
+    promptString.focus();
+  });
+});
+
+let locationPick = document.querySelectorAll(".box2");
+locationPick.forEach((item) => {
+  item.addEventListener("click", () => {
+    appendToQuery(item.dataset.id);
+    console.log(item.dataset.id);
+    locationCount++;
+    totalCount++;
+    locationCounter.textContent = locationCount;
+    locationMiniCounter.textContent = locationCount;
+    totalCounter.textContent = totalCount;
+    promptString.focus();
+  });
+});
+
+let themesPick = document.querySelectorAll(".box3");
+themesPick.forEach((item) => {
+  item.addEventListener("click", () => {
+    appendToQuery(item.dataset.id);
+    console.log(item.dataset.id);
+    themesCount++;
+    totalCount++;
+    themesCounter.textContent = themesCount;
+    themeMiniCounter.textContent = themesCount;
+    totalCounter.textContent = totalCount;
+    promptString.focus();
+  });
+});
+
+let designPick = document.querySelectorAll(".box4");
+designPick.forEach((item) => {
+  item.addEventListener("click", () => {
+    appendToQuery(item.dataset.id);
+    console.log(item.dataset.id);
+    designCount++;
+    totalCount++;
+    designCounter.textContent = designCount;
+    designMiniCounter.textContent = designCount;
+    totalCounter.textContent = totalCount;
+    promptString.focus();
+  });
+});
+
+let lightningPick = document.querySelectorAll(".box5");
+lightningPick.forEach((item) => {
+  item.addEventListener("click", () => {
+    appendToQuery(item.dataset.id);
+    console.log(item.dataset.id);
+    lightningCount++;
+    totalCount++;
+    lightningCounter.textContent = lightningCount;
+    lightningMiniCounter.textContent = lightningCount;
+    totalCounter.textContent = totalCount;
+    promptString.focus();
+  });
+});
+
+let colorsPick = document.querySelectorAll(".box6");
+colorsPick.forEach((item) => {
+  item.addEventListener("click", () => {
+    appendToQuery(item.dataset.id);
+    console.log(item.dataset.id);
+    colorsCount++;
+    totalCount++;
+    colorsCounter.textContent = colorsCount;
+    colorsMiniCounter.textContent = colorsCount;
+    totalCounter.textContent = totalCount;
+    promptString.focus();
+  });
+});
+
+let cameraPick = document.querySelectorAll(".box7");
+cameraPick.forEach((item) => {
+  item.addEventListener("click", () => {
+    appendToQuery(item.dataset.id);
+    console.log(item.dataset.id);
+    cameraCount++;
+    totalCount++;
+    cameraCounter.textContent = cameraCount;
+    cameraMiniCounter.textContent = cameraCount;
+    totalCounter.textContent = totalCount;
+    promptString.focus();
+  });
+});
+
+let artistsPick = document.querySelectorAll(".box8");
+artistsPick.forEach((item) => {
+  item.addEventListener("click", () => {
+    appendToQuery(item.dataset.id);
+    console.log(item.dataset.id);
+    artistsCount++;
+    totalCount++;
+    artistsCounter.textContent = artistsCount;
+    artistMiniCounter.textContent = artistsCount;
     totalCounter.textContent = totalCount;
     promptString.focus();
   });
