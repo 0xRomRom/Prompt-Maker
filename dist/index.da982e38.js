0a116bcf664c99d10145b79aa6a126e33bfc62bc
0xromrom <roman0811@live.nl>
2022-09-07 10:25:10 -0700

    Changes to server

--- dist/index.da982e38.js
+++ dist/index.da982e38.js
@@ -551,31 +551,26 @@ const app = express();
 const endpointSecret = "whsec_9ac22201943d5aee4ffb292e0b049e5d93eb3f7a32388b5f7064f6fdef81cb4a";
 app.post("/webhook", express.raw({
     type: "application/json"
-}), (req, res)=>{
-    const sig = req.headers["stripe-signature"];
-    let data;
-    let eventType;
-    if (endpointSecret) {
-        let event;
-        try {
-            event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-            console.log("Webhook verified");
-        } catch (err) {
-            console.log(`Webhook Error: ${err.message}`);
-            res.status(400).send(`Webhook Error: ${err.message}`);
-            return;
-        }
-        data = event.data.object;
-        eventType = event.type;
-    } else {
-        data = req.body.data.object;
-        eventType = req.body.type;
+}), (request, response)=>{
+    const sig = request.headers["stripe-signature"];
+    let event;
+    try {
+        event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+        response.status(400).send(`Webhook Error: ${err.message}`);
+        return;
     }
-    if (eventType.type === "checkout.session.completed") {
-        const paymentIntent = eventType.data.object;
+    // Handle the event
+    switch(event.type){
+        case "checkout.session.completed":
+            const session = event.data.object;
+            break;
+        // ... handle other event types
+        default:
+            console.log(`Unhandled event type ${event.type}`);
     }
-    // Return a 200 res to acknowledge receipt of the event
-    res.send();
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
 });
 app.listen(4242, ()=>console.log("Running on port 4242"));
 
